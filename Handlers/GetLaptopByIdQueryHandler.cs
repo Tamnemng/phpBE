6183bd5ddaa885dbeb2482c73c05bdb761242d50
NGUYỄN PHÚC THỊNH <119058000+hieu79115@users.noreply.github.com>
2025-03-21 03:15:22 +0700

    fix GetLaptopById

--- Handlers/GetLaptopByIdQueryHandler.cs
+++ Handlers/GetLaptopByIdQueryHandler.cs
@@ -2,17 +2,19 @@
 using Dapr.Client;
 using MediatR;
 
-public class GetLaptopByIdQueryHandler : IRequestHandler<GetLaptopByIdQuery, string>
+public class GetLaptopByIdQueryHandler : IRequestHandler<GetLaptopByIdQuery, Dictionary<string, object>>
 {
     private readonly DaprClient _daprClient;
+    private const string STORE_NAME = "statestore";
 
-    private const string STORE_NAME =  "statestore";
-
-    public GetLaptopByIdQueryHandler(DaprClient daprClient){
+    public GetLaptopByIdQueryHandler(DaprClient daprClient)
+    {
         _daprClient = daprClient;
     }
 
-    public async Task<string> Handle(GetLaptopByIdQuery request, CancellationToken cancellationToken){
-        return await _daprClient.GetStateAsync<string>(STORE_NAME, request.Id, cancellationToken: cancellationToken);
+    public async Task<Dictionary<string, object>> Handle(GetLaptopByIdQuery request, CancellationToken cancellationToken)
+    {
+        var data = await _daprClient.GetStateAsync<Dictionary<string, object>>(STORE_NAME, request.Id, cancellationToken: cancellationToken);
+        return data ?? throw new Exception($"Laptop với ID '{request.Id}' không tồn tại.");
     }
 }
